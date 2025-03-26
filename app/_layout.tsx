f3b6472a0a06e2c7990ae1e425d6bf81f4f5b8a7
Favour Akande <favourakande1@gmail.com>
2025-03-26 15:18:23 +0100

    Changed header colour

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,74 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
-import { handleRecurringUpdates, setupDatabase } from "./database";
+import { handleRecurringUpdates, initializeDatabase } from "./database";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
+import { BudgetProvider } from './context/BudgetContext';
 
 export default function RootLayout() {
 
   useEffect(() => {
     const initializeDB = async () => {
-      await setupDatabase();
+      await initializeDatabase();
     };
     initializeDB();
     handleRecurringUpdates();
   }, []);
 
   return (
-          <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="addPurchaseScreen" options={{ headerShown: false }} />
-            <Stack.Screen name="plannedPurchasesScreen" options={{ headerShown: false }} />
-            <Stack.Screen name="addIncomeScreen" options={{ headerShown: false }} />
-            <Stack.Screen name="addExpenseScreen" options={{ headerShown: false }} />
-          </Stack>
+    <BudgetProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Budget Tracker',
+            headerStyle: {
+              backgroundColor: '#043927',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen
+          name="addIncomeScreen"
+          options={{
+            title: 'Add Income',
+            headerStyle: {
+              backgroundColor: '#043927',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen
+          name="addExpenseScreen"
+          options={{
+            title: 'Add Expense',
+            headerStyle: {
+              backgroundColor: '#043927',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen
+          name="addPurchaseScreen"
+          options={{
+            title: 'Add Purchase',
+            headerStyle: {
+              backgroundColor: '#043927',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+        <Stack.Screen
+          name="plannedPurchasesScreen"
+          options={{
+            title: 'Planned Purchases',
+            headerStyle: {
+              backgroundColor: '#043927',
+            },
+            headerTintColor: '#fff',
+          }}
+        />
+      </Stack>
+    </BudgetProvider>
   );
 }
